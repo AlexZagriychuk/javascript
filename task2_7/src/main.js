01de3b8ad7b87ecd1c9fe9da1fb15fc3829ecc79
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-04-07 00:59:07 -0700

    Task 2.7 JS for filtering by country name and region

--- task2_7/src/main.js
+++ task2_7/src/main.js
@@ -1,9 +1,10 @@
 import CountriesAPI from "./api/CountriesAPI.js";
 import ElementUtils from "./ElementUtils.js";
-
+import JsUtils from "./JsUtils.js";
 
 const countriesParsedData = await CountriesAPI.getCountriesParsedData()
 const countriesListElem = document.querySelector(".search-results")
+const countrySearchInputElem = document.getElementById("country-search-input")
 const regionFilterSelectElem = document.getElementById("region-filter-select")
 
 
@@ -42,8 +43,37 @@ function populateRegionFilterSelectOptions(countriesParsedData) {
     })
 }
 
+// Filtering by both country name (case insensitive) and region value at the same time
+function handleFilterChange(event) {
+    let countryNameFilter = countrySearchInputElem.value.trim()
+    let regionFilterSelectedOption = regionFilterSelectElem.options[regionFilterSelectElem.selectedIndex]
+    let regionFilterValue = regionFilterSelectedOption.value.trim()
+    let regionFilterText = regionFilterSelectedOption.text.trim()
+
+    if(regionFilterText === "") {
+        // Select default option with value "" (and text "Filter by Region") if option with empty text is selected (region filter is deactivated)
+        regionFilterSelectElem.value = ""
+        regionFilterValue = ""
+    }   
+
+    let countryCards = Array.from(countriesListElem.querySelectorAll(".country-card"))
+    // Iterating over the cached data (countriesParsedData) to figure out which country items need to be hidden (instead of reading data from the DOM elements which would be slower)
+    countriesParsedData.forEach((country, index) => {
+        let countryCardIsCurrentlyDisplayed = countryCards[index].style.display !== "none"
+        let countryCardShouldBeVisible = country.name.toLowerCase().includes(countryNameFilter.toLowerCase()) 
+                                        && (regionFilterValue === "" || country.region === regionFilterText) 
+
+        if(countryCardShouldBeVisible && !countryCardIsCurrentlyDisplayed) {
+            countryCards[index].style.display = ""
+        } else if (!countryCardShouldBeVisible && countryCardIsCurrentlyDisplayed) {
+            countryCards[index].style.display = "none"
+        }
+    }) 
+}
 
 
 renderCountries(countriesParsedData)
 populateRegionFilterSelectOptions(countriesParsedData)
+countrySearchInputElem.addEventListener("input", JsUtils.debounce((event) => handleFilterChange(event)))
+regionFilterSelectElem.addEventListener("change", handleFilterChange)
 
