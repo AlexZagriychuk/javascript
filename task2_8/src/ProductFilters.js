78470142bb3df67bdaf6bbfdcd52cb0d55a77727
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-05-10 12:42:52 -0700

    Task 2.8 - JS to filter by company name on the product page

--- task2_8/src/ProductFilters.js
+++ task2_8/src/ProductFilters.js
@@ -1,16 +1,33 @@
 import JsUtils from "./utils/JsUtils.js"
+import ElementUtils from "./utils/ElementUtils.js";
 
 
 export default class ProductFilters {
     constructor(productsData) {
         // Binding class methods to the class instance
         this.handleProductNameFilterChange = this.handleProductNameFilterChange.bind(this);
+        this.handleCompanyNameFilterChange = this.handleCompanyNameFilterChange.bind(this);
         this.handleProductFilterChange = this.handleProductFilterChange.bind(this);
+
+        this.productNameFilterValue = ""
+        this.companyNameFilterValue = "All"
 
         this.productsData = productsData
         this.productElements = Array.from(document.querySelectorAll(".product"))
         this.productNameFilterElem = document.getElementById("products-filter-product-name")
+        this.productCompaniesListFilterElem = document.querySelector(".products-filter-company-list")
 
+        // Generate list of companies names filters based on the productsData (+ "All" to show all companies)
+        let companyNameFilterElem = ElementUtils.createAndAppendElement(this.productCompaniesListFilterElem, "li", "active", "All")
+        companyNameFilterElem.addEventListener("click", this.handleCompanyNameFilterChange)
+
+        let companyNames = new Set(productsData.map(product => product.brand))
+        companyNames.forEach(companyName => {
+            let companyNameFilterElem = ElementUtils.createAndAppendElement(this.productCompaniesListFilterElem, "li", "", companyName)
+            companyNameFilterElem.addEventListener("click", this.handleCompanyNameFilterChange)
+        })
+
+        // Add event listeners
         this.productNameFilterElem.addEventListener("input", JsUtils.debounce(this.handleProductNameFilterChange))
     }
 
@@ -20,13 +37,25 @@ export default class ProductFilters {
         this.handleProductFilterChange()
     }
 
+    handleCompanyNameFilterChange(event) {     
+        const clickedCompanyNameFilterElement = event.target
+        this.companyNameFilterValue = clickedCompanyNameFilterElement.innerText
+
+        const companyNameFilterElements = Array.from(this.productCompaniesListFilterElem.querySelectorAll("li"))
+        companyNameFilterElements.forEach(companyNameFilterElem => companyNameFilterElem.classList.remove("active"))
+        clickedCompanyNameFilterElement.classList.add("active")
+
+        this.handleProductFilterChange()
+    }
+
     handleProductFilterChange() {
         this.productElements.forEach((productElem, index) => {
-            // Instead of reading a product name value from the DOM we read it from the productsData (which is used to render this list of products)
-            let productName = this.productsData[index].name
-            let shouldBeVisibleByNameFilter = productName.toLowerCase().includes(this.productNameFilterValue.toLowerCase())
+            // Instead of reading the product data from the DOM, to increase speed we read it from the productsData arr (which is used to render this list of products)
+            let productData = this.productsData[index]
+            let satisfyProductNameFilter = productData.name.toLowerCase().includes(this.productNameFilterValue.toLowerCase())
+            let satisfyCompanyNameFilter = this.companyNameFilterValue.toLowerCase() === "all" || this.companyNameFilterValue === productData.brand
 
-            productElem.style.display = shouldBeVisibleByNameFilter ? "" : "none"
+            productElem.style.display = satisfyProductNameFilter && satisfyCompanyNameFilter ? "" : "none"
         })
     }
 }
--- task2_8/src/api/productsApi.js
+++ task2_8/src/api/productsApi.js
@@ -2,7 +2,7 @@ const productsData = [
     {
         "id": 1,
         "name": "Morabo Sofa",
-        "brand": "ikea",
+        "brand": "Ikea",
         "price": "699.00",
         "imageURL": "resources/ikea-01-morabo.avif",
         "isFeatured": true
@@ -10,7 +10,7 @@ const productsData = [
     {
         "id": 2,
         "name": "Malm Bed",
-        "brand": "ikea",
+        "brand": "Ikea",
         "price": "349.00",
         "imageURL": "resources/ikea-02-malm-bed.avif",
         "isFeatured": true
@@ -18,7 +18,7 @@ const productsData = [
     {
         "id": 3,
         "name": "Kallax Shelf Unit",
-        "brand": "ikea",
+        "brand": "Ikea",
         "price": "129.00",
         "imageURL": "resources/ikea-03-kallax-shelf-unit.avif",
         "isFeatured": true
@@ -26,7 +26,7 @@ const productsData = [
     {
         "id": 4,
         "name": "Morabo Sofa 2",
-        "brand": "ikea",
+        "brand": "Ikea2",
         "price": "799.00",
         "imageURL": "resources/ikea-01-morabo.avif",
         "isFeatured": false
@@ -34,15 +34,15 @@ const productsData = [
     {
         "id": 5,
         "name": "Malm Bed 2",
-        "brand": "ikea",
+        "brand": "Ikea2",
         "price": "449.00",
         "imageURL": "resources/ikea-02-malm-bed.avif",
         "isFeatured": false
     },
     {
         "id": 6,
-        "name": "Kallax Shelf Unit 2",
-        "brand": "ikea",
+        "name": "Kallax Shelf Unit 3",
+        "brand": "Ikea3",
         "price": "229.00",
         "imageURL": "resources/ikea-03-kallax-shelf-unit.avif",
         "isFeatured": false
