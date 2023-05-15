9191c997f38fb7a177ac7cdbd39439589220cc6c
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-05-14 20:20:09 -0700

    Task 2.8 - update the furniture content and pics

--- task2_8/src/api/productsApi.js
+++ task2_8/src/api/productsApi.js
@@ -13,7 +13,7 @@ const productsData = [
         "brand": "Ikea",
         "price": "349.00",
         "imageURL": "/task2_8/resources/ikea-02-malm-bed.avif",
-        "isFeatured": true
+        "isFeatured": false
     },
     {
         "id": 3,
@@ -21,30 +21,62 @@ const productsData = [
         "brand": "Ikea",
         "price": "129.00",
         "imageURL": "/task2_8/resources/ikea-03-kallax-shelf-unit.avif",
-        "isFeatured": true
+        "isFeatured": false
     },
     {
         "id": 4,
-        "name": "Morabo Sofa 2",
-        "brand": "Ikea2",
-        "price": "799.00",
-        "imageURL": "/task2_8/resources/ikea-01-morabo.avif",
+        "name": "Desky Single Sit Stand Desk",
+        "brand": "Desky",
+        "price": "579.00",
+        "imageURL": "/task2_8/resources/Desky-01-Single Sit Stand Desk.webp",
         "isFeatured": false
     },
     {
         "id": 5,
-        "name": "Malm Bed 2",
-        "brand": "Ikea2",
-        "price": "449.00",
-        "imageURL": "/task2_8/resources/ikea-02-malm-bed.avif",
+        "name": "Desky Pro Ergonomic Chair",
+        "brand": "Desky",
+        "price": "599.00",
+        "imageURL": "/task2_8/resources/Desky-02-Pro Ergonomic Chair.webp",
         "isFeatured": false
     },
     {
         "id": 6,
-        "name": "Kallax Shelf Unit 3",
-        "brand": "Ikea3",
-        "price": "229.00",
-        "imageURL": "/task2_8/resources/ikea-03-kallax-shelf-unit.avif",
+        "name": "Zamora Arch Metal Wall Mirror",
+        "brand": "Greyleigh",
+        "price": "299.99",
+        "imageURL": "/task2_8/resources/Greyleigh-01-Zamora+Arch+Metal+Wall+Mirror.webp",
+        "isFeatured": false
+    },
+    {
+        "id": 7,
+        "name": "Americus Upholstered Bed",
+        "brand": "Greyleigh",
+        "price": "679.99",
+        "imageURL": "/task2_8/resources/Greyleigh-02-Americus+Upholstered+Bed.webp",
+        "isFeatured": true
+    },
+    {
+        "id": 8,
+        "name": "Cottage 6-Piece Dining Set - Grey",
+        "brand": "Leons",
+        "price": "1199.00",
+        "imageURL": "/task2_8/resources/leons-01-Cottage 6-Piece Dining Set.webp",
+        "isFeatured": false
+    },
+    {
+        "id": 9,
+        "name": "Atomic Coffee Table - Brown Cherry",
+        "brand": "Leons",
+        "price": "379.00",
+        "imageURL": "/task2_8/resources/leons-02-Atomic Coffee Table - Brown Cherry.webp",
+        "isFeatured": true
+    },
+    {
+        "id": 10,
+        "name": "Dallas Glider Recliner",
+        "brand": "Leons",
+        "price": "899.00",
+        "imageURL": "/task2_8/resources/leons-03-Dallas Glider Recliner.webp",
         "isFeatured": false
     }
 ]
